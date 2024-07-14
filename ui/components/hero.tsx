11a1198802a03ca19cffc5895072b666c5616762
Saúl García Céspedes <saul@dixwe.com>
2024-07-14 06:56:23 +0200

    fix: typo

--- ui/components/hero.tsx
+++ ui/components/hero.tsx
@@ -9,7 +9,7 @@ export default function HeroSection() {
         <div className="flex items-center justify-between gap-2">
           <div className="flex flex-1 flex-col items-center">
             <h1 className="text-center text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">Fast, simple decentralized social payments</h1>
-            <p className="mt-6 max-w-[600px] text-center text-muted-foreground md:text-xl">Send and recieve payments from your friends</p>
+            <p className="mt-6 max-w-[600px] text-center text-muted-foreground md:text-xl">Send and receive payments from your friends</p>
           </div>
         </div>
         <div className="my-5 flex justify-center gap-5">
